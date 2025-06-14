2f18b17899675906a730f7650065912642abdacc
Rehmat Qureshi <rehmat.q@skillgigs.com>
2025-06-14 05:21:44 -0700

    Backend_test

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,7 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+    
 });
 
 export type Schema = ClientSchema<typeof schema>;
